17f69b91e3dc950441afccadfdf8068313e983ce
Richard Walker <walker@omnisterra.com>
2014-07-24 16:21:31 -0700

    Working on spice raw file data extractor

--- rawtest/spread.c
+++ rawtest/spread.c
@@ -5,30 +5,16 @@
 #include <stdio.h>
 #include <string.h>
 
-char *skipblanks(char *s) {
-   for (; *s==' ' && *s !='\0'; s++) {
-     ;
-   }
-   return s;
-}
+char *skipblanks(char *s);
+char *prefix(char *s, char *p);
 
-// check if string s is prefixed by string p
-// return s with p stripped, or NULL if not matched
-
-char *prefix(char *s, char *p) {
-   if (!strncmp(s, p, strlen(p))) {
-      return(skipblanks(s+strlen(p)));
-   } else {
-      return NULL;
-   }
-}
 
 int read_header(int *nvars, int *npts, int *cflag) {
    char *arg;
    char s[1024];   
    int err=1;
    printf("--------------------------------------------\n");
-   while(fgets(s, 1024, stdin) != NULL) {
+   while(fgets(s, sizeof(s), stdin) != NULL) {
 	if (arg=prefix(s, "Title:")) {
 	    printf("Title: %s", arg);
 	    err=0;
@@ -76,6 +62,7 @@ read_bin(int nvars, int npts, int cflag) {
   int i,v,p;
   int total;
   double d;
+  double e;
   char bytes[8];
 
   if (cflag) {
@@ -87,27 +74,69 @@ read_bin(int nvars, int npts, int cflag) {
   printf("in read_bin, cflag=%d, total=%d\n",cflag, total);
 
   for (p=0; p<npts; p++) {
-      for (v=0; v<nvars; v++) {
-	   fread(&bytes, 8, 1, stdin);
-	   d = *((double*)bytes);
-	   printf("%g ",d);
+
+      // get independent variable
+      fread(&bytes, sizeof(bytes), 1, stdin);
+      d = *((double*)bytes);
+      fread(&bytes, sizeof(bytes), 1, stdin);	// throw away complex part
+      printf("%g ",d);
+
+      // read simulation vars
+      for (v=1; v<nvars; v++) {
+	  if (!cflag) {
+	        fread(&bytes, sizeof(bytes), 1, stdin);
+	        d = *((double*)bytes);
+      		printf("%g ",d);
+	  } else {
+	        fread(&bytes, sizeof(bytes), 1, stdin);
+	        d = *((double*)bytes);	// real
+	        fread(&bytes, sizeof(bytes), 1, stdin);
+	        e = *((double*)bytes);	// imag
+      		printf("(%g,%g) ",d,e);
+
+	  }
       }
-      printf("\n",d);
+      printf("\n");
   }
 }
+
+// table[pts][var][0:1]
 
 main() {
    int i,x;
 
    int nvars, npts;
    int complexflag;
+   double ** table;
 
    while(!feof(stdin)) {
        if (feof(stdin)) break;
        if (read_header(&nvars, &npts, &complexflag) == 0) {
+	   // malloc big array of arrays here
 	   read_bin(nvars, npts, complexflag);
+	   break;
        } else {
-	  break;
+	   exit(0);
        }
+   }
+   // print out big array here
+   exit(1);
+}
+
+char *skipblanks(char *s) {
+   for (; *s==' ' && *s !='\0'; s++) {
+     ;
+   }
+   return s;
+}
+
+// check if string s is prefixed by string p
+// return s with p stripped, or NULL if not matched
+
+char *prefix(char *s, char *p) {
+   if (!strncmp(s, p, strlen(p))) {
+      return(skipblanks(s+strlen(p)));
+   } else {
+      return NULL;
    }
 }
